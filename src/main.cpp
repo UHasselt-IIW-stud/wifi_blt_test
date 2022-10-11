989205beb22f4127a0170f5f9c454d05fae1782f
T. Vandenryt <thijs.vandenryt@uhasselt.be>
2022-10-11 18:39:33 +0200

    Initial commit - Wifi Connect

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,43 @@
 #include <Arduino.h>
+#include "WiFi.h"
 
+const char* SSID = "test";
+const char* PASSWORD = "Test1234";
+/*
+void setup() { 
+  Serial.begin(115200);
+  //WiFi.mode(WIFI_STA);
+  WiFi.begin(SSID, PASSWORD);
+
+  Serial.print(F("Connecting to WiFi .."));
+  while (WiFi.status() != WL_CONNECTED) {
+    Serial.print(F('.'));
+    delay(500);
+  }
+  Serial.printf("Connected to \"%S\", with ip: ",SSID);
+  Serial.println(WiFi.localIP());
+}
+*/
+
+ 
 void setup() {
-  // put your setup code here, to run once:
+ WiFi.disconnect();
+
+  Serial.begin(115200);
+  Serial.printf("Connecting to: \"%S\" with code: \"%S\".\n",SSID,PASSWORD);
+ 
+  WiFi.begin(SSID, PASSWORD);
+   
+  Serial.print("Connecting to WiFi");
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+ 
+  Serial.println("\nConnected to the WiFi network");
+ 
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+
 }
